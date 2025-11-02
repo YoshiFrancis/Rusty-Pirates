c90fc8332ceb2f381984cbe3ec936b300bd9d89e
YoshiFrancis <francis.ozua@gmail.com>
2025-11-02 01:28:44 -0500

    moved everything to rust workspaces

--- rusty-pirate/src/ship.rs
+++
@@ -1,8 +0,0 @@
-
-
-
-// receive from user and redirect to wherever
-
-// receive from cabin and redirect to wherever
-
-// handle messages from default cabin (like if to kick out user or whatever)
--- src/default_cabin.rs
+++
@@ -1,3 +0,0 @@
-// act as a hub
-// holds registry of all players within the ship
-// holds registry of all cabins connected to the ship
