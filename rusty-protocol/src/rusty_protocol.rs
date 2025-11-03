3fa8dcee5e24438071d1eb7444a24a969e7faa77
YoshiFrancis <francis.ozua@gmail.com>
2025-11-03 01:18:51 -0500

    wrote testing for parsing messages (bytes to message type)
    also did some moving around of functions

--- rusty-protocol/src/rusty_protocol.rs
+++ rusty-protocol/src/rusty_protocol.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum RPMessageType {
   Text = 0,
   Fneed = 1,
@@ -53,7 +53,11 @@ pub fn bytes_to_message(bytes : &[u8]) -> Result<Message, MessageErr> {
   let fs : u8 = 34;
 
   if bytes.len() < 6 {
-    return Err(MessageErr { err: (String::from("Not enough bytes to get four byte message and end double FS")) })
+    return Err(MessageErr { err: (String::from("Not enough bytes")) })
+  }
+
+  if bytes[bytes.len() - 1] != fs || bytes[bytes.len() - 2] != fs {
+    return Err(MessageErr { err: (String::from("No ending double FS")) })
   }
 
   let mut iter = bytes[0..bytes.len() - 2].split(|&byte| byte == fs).enumerate();
@@ -63,9 +67,7 @@ pub fn bytes_to_message(bytes : &[u8]) -> Result<Message, MessageErr> {
     .try_into()
     .expect("Failed to convert first 4 bytes of message to u32"));
 
-  if bytes[bytes.len() - 1] != fs && bytes[bytes.len() - 2] != fs {
-    return Err(MessageErr { err: (String::from("Did not receive double FS at the end of message")) })
-  }
+  
 
   let mut args  = vec![];
   args.resize(size, String::from(""));
@@ -75,49 +77,112 @@ pub fn bytes_to_message(bytes : &[u8]) -> Result<Message, MessageErr> {
     ).expect("Could not convert Vec<u8> to String");
   };
 
-  match u32_to_rptype(rp_type_u32) {
+  match RPMessageType::try_from(rp_type_u32) {
     Ok(rp_type) => Ok(Message { rp_type, args }),
     Err(error) => Err(error)
   }
 
 }
 
-fn u32_to_rptype(rp_type_id : u32) -> Result<RPMessageType, MessageErr> {
-  match rp_type_id {
-    0 => Ok(RPMessageType::Text),
-    1 => Ok(RPMessageType::Fneed),
-    2 => Ok(RPMessageType::Freq),
-    3 => Ok(RPMessageType::Pdirectory),
-    4 => Ok(RPMessageType::Cdirectory),
-    5 => Ok(RPMessageType::Pnamereq),
-    6 => Ok(RPMessageType::Cinforeq),
-    7 => Ok(RPMessageType::Pinput),
-    8 => Ok(RPMessageType::Join),
-    9 => Ok(RPMessageType::Leave),
-    10 => Ok(RPMessageType::Info),
-    11 => Ok(RPMessageType::Name),
-    _ => Err(MessageErr {err: 
-      format!("failed to parse id to Rusty Protocol message type: {}", rp_type_id)
-      })
+impl TryFrom<u32> for RPMessageType {
+  type Error = MessageErr;
+  fn try_from(n: u32) -> Result<Self, MessageErr> {
+      match n {
+          0 => Ok(Self::Text),
+          1 => Ok(Self::Fneed),
+          2 => Ok(Self::Freq),
+          3 => Ok(Self::Pdirectory),
+          4 => Ok(Self::Cdirectory),
+          5 => Ok(Self::Pnamereq),
+          6 => Ok(Self::Cinforeq),
+          7 => Ok(Self::Pinput),
+          8 => Ok(Self::Join),
+          9 => Ok(Self::Leave),
+          10 => Ok(Self::Info),
+          _ => Err(MessageErr{err: String::from("failed")}),
+      }
   }
 }
 
-impl TryFrom<u8> for RPMessageType {
-    type Error = ();
-    fn try_from(n: u8) -> Result<Self, Self::Error> {
-        match n {
-            0 => Ok(Self::Text),
-            1 => Ok(Self::Fneed),
-            2 => Ok(Self::Freq),
-            3 => Ok(Self::Pdirectory),
-            4 => Ok(Self::Cdirectory),
-            5 => Ok(Self::Pnamereq),
-            6 => Ok(Self::Cinforeq),
-            7 => Ok(Self::Pinput),
-            8 => Ok(Self::Join),
-            9 => Ok(Self::Leave),
-            10 => Ok(Self::Info),
-            _ => Err(()),
-        }
+#[cfg(test)]
+mod tests {
+    use std::io::Bytes;
+
+    use super::*;
+
+    const FS : u8 = 34;
+
+    #[test]
+    fn parse_message_ok() {
+      let expected_message = Message {
+        rp_type: RPMessageType::Cdirectory,
+        args: vec![String::from("testing1"), String::from("testing2")]
+      };
+
+      let cdirectory_type : u32 = RPMessageType::Cdirectory as u32;
+
+      let mut message_bytes : Vec<u8> = vec![];
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing1").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing2").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.push(FS);
+
+      let actual_message = bytes_to_message(&message_bytes).expect("Failed to parse ok message");
+      assert_eq!(actual_message.rp_type, expected_message.rp_type);
+      assert_eq!(actual_message.args, expected_message.args);
+
     }
+
+    #[test]
+    #[should_panic]
+    fn parse_message_fail_bad_fs() {
+      let cdirectory_type : u32 = RPMessageType::Cdirectory as u32;
+      let mut message_bytes : Vec<u8> = vec![];
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing1").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing2").as_bytes());
+      message_bytes.push(FS);
+
+      match bytes_to_message(&message_bytes) {
+        Err(_) => panic!("Correct panic"),
+        Ok(_) => println!("Passed with no double fs at end: {:#?}", message_bytes)
+      }
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_message_fail_rp_type() {
+      let cdirectory_type : u32 = 100;
+      let mut message_bytes : Vec<u8> = vec![];
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing1").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing2").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.push(FS);
+
+      bytes_to_message(&message_bytes).unwrap();
+    }
+
+    #[test]
+    fn decode_message_ok() {
+      
+    }
+
+    #[test]
+    fn decode_message_fail() {
+
+    }
+
+    #[test] 
+    fn message_in_and_out() {
+
+    }
+
 }
