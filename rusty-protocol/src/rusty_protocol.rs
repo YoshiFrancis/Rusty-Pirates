bcbd01b212d0fc88a8e2f5ef742b627a20aee3a9
YoshiFrancis <francis.ozua@gmail.com>
2025-11-09 23:33:18 -0500

    added checck function and tests
    used to check whether the current bytes can be parsed to message and returns error on failure

--- rusty-protocol/src/rusty_protocol.rs
+++ rusty-protocol/src/rusty_protocol.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{cmp::max, fmt::Debug};
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum RPMessageType {
@@ -16,6 +16,14 @@ pub enum RPMessageType {
   Leave = 9,
   // client response
   Name = 11,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum ParseResult {
+  NotEnoughBytes,
+  IncompleteEnd,
+  Invalid(usize), // invalid beginning or end
+  Complete(usize)
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -47,6 +55,42 @@ pub fn encode_message(message : Message) -> Vec<u8> {
   bytes.push(fs);
 
   bytes
+}
+
+pub fn check(bytes : &[u8], starting_pos : usize) -> Option<ParseResult> {
+  let fs : u8 = 34;
+
+  if bytes.len() < 6 {
+    return Some(ParseResult::NotEnoughBytes);
+  }
+
+  // its ugly,... I KNOW D:
+  if starting_pos == 0 {
+    let mut found = false;
+    for i in 0..bytes.len() - 3 {
+      let rp_type_u32 = u32::from_le_bytes(bytes[i..i+4].try_into().unwrap());
+      if let Ok(_) = RPMessageType::try_from(rp_type_u32) {
+        if i == 0 {
+          found = true;
+          break;
+        } else {
+          return Some(ParseResult::Invalid(i)); // found beginning of valid point
+        }
+      }
+    }
+    if !found {
+      return Some(ParseResult::Invalid(bytes.len())); // found no valid starting byte message for the message
+    }
+  }
+
+  let starting_pos = max(starting_pos, 5);
+
+  for idx in starting_pos..bytes.len() {
+    if bytes[idx-1] == fs && bytes[idx] == fs {
+      return Some(ParseResult::Complete(idx+1));
+    }
+  }
+  return Some(ParseResult::IncompleteEnd);
 }
 
 pub fn decode_bytes(bytes : &[u8]) -> Result<Message, MessageErr> {
@@ -111,6 +155,75 @@ mod tests {
     use super::*;
 
     const FS : u8 = 34;
+
+    #[test]
+    fn check_complete() {
+      let cdirectory_type : u32 = RPMessageType::Cdirectory as u32;
+      let mut message_bytes : Vec<u8> = vec![];
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing1").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing2").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.push(FS);
+
+      let original_length = message_bytes.len();
+
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::Complete(original_length));
+
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing1").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing2").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.push(FS);
+
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::Complete(original_length));
+      assert_eq!(check(&message_bytes, original_length).unwrap(), ParseResult::Complete(message_bytes.len()));
+    }
+
+    #[test]
+    fn check_invalid_beginning() {
+      let invalid_type : u32 = 100;
+      
+      let mut message_bytes : Vec<u8> = vec![];
+      message_bytes.extend_from_slice(&invalid_type.to_le_bytes());
+      message_bytes.push(FS);
+      message_bytes.push(FS);
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::Invalid(6));
+      let cdirectory_type : u32 = RPMessageType::Cdirectory as u32;
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::Invalid(6));
+    }
+
+    #[test]
+    fn check_not_enough_bytes() {
+      let cdirectory_type : u32 = RPMessageType::Cdirectory as u32;
+      let mut message_bytes : Vec<u8> = vec![];
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::NotEnoughBytes);
+      message_bytes.push(FS);
+      message_bytes.push(FS);
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::Complete(message_bytes.len()));
+    }
+
+    #[test]
+    fn check_incomplete_end() {
+      let cdirectory_type : u32 = RPMessageType::Cdirectory as u32;
+      let mut message_bytes : Vec<u8> = vec![];
+      message_bytes.extend_from_slice(&cdirectory_type.to_le_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing1").as_bytes());
+      message_bytes.push(FS);
+      message_bytes.extend_from_slice(&String::from("testing2").as_bytes());
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::IncompleteEnd);
+      message_bytes.push(FS);
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::IncompleteEnd);
+      message_bytes.push(FS);
+      assert_eq!(check(&message_bytes, 0).unwrap(), ParseResult::Complete(message_bytes.len()));
+    }
 
     #[test]
     fn parse_message_ok() {
